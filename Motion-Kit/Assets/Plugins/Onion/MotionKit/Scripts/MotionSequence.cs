4b92a087683f8aaf1f4479d2f445bf7afb0c75cd
Seungbin <seungbin0619@naver.com>
2025-10-31 22:25:58 +0900

    refactor: 추후 기능 확장을 위해 Sequence로 변경

--- Motion-Kit/Assets/Plugins/Onion/MotionKit/Scripts/MotionSequence.cs
+++ Motion-Kit/Assets/Plugins/Onion/MotionKit/Scripts/MotionSequence.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using PrimeTween;
 using UnityEngine;
 
 namespace Onion.MotionKit {
@@ -10,10 +11,16 @@ namespace Onion.MotionKit {
 
         [SerializeReference]
         public List<MotionTrack> tracks = new();
+        private Sequence _sequence;
 
         public void Play() {
+            if (_sequence.isAlive) {
+                _sequence.Complete();    
+            }
+
+            _sequence = Sequence.Create();
             foreach (var track in tracks) {
-                track.Create();
+                _sequence.Group(track.Create());
             }
         }
     }
