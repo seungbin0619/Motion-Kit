23528e7b46c1b7a058344cf3bb8c0536a9e50911
Seungbin <seungbin0619@naver.com>
2025-11-05 20:12:30 +0900

    feat: 이동 애니메이션 추가

---
+++ Motion-Kit/Assets/Plugins/Onion/MotionKit/Scripts/Clips/MoveClip.cs
@@ -0,0 +1,19 @@
+using PrimeTween;
+using UnityEngine;
+
+namespace Onion.MotionKit {
+    [MotionClipMenu("Move")]
+    [CreateAssetMenu(menuName = "Animation/Motion Kit/Move Clip")]
+    public sealed class MoveClip : MotionClipWithValue<Vector3> {
+        [SerializeField]
+        private bool _isLocal;
+
+        protected override Tween Create(Component target, TweenSettings<Vector3> _settings) {
+            if (target is not Transform transform) return default;
+
+            return _isLocal
+                ? Tween.LocalPosition(transform, _settings)
+                : Tween.Position(transform, _settings);
+        }
+    }
+}
